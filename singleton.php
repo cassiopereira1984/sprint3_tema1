4e671d2eb352d22d0648ce2e629971636444d760
cassiopereira1984 <cassiopereira1984@gmail.com>
2024-04-09 13:35:51 +0200

    adaptacion al enunciado

--- singleton.php
+++ singleton.php
@@ -14,14 +14,15 @@ invocar al constructor, pero no a los otros objetos.
 Repasa el código cliente y sustituye todas las llamadas directas al constructor de la instancia 
 Singleton por llamadas a su método de creación estático. */
 
-class Singleton {
+class Tigger {
     //Añade un campo estático privado a la clase para almacenar la instancia Singleton.
     private static $variable;
+    private static $count = 0;
 
     /*Declara el constructor de clase como privado. El método estático de la clase seguirá siendo capaz de 
     invocar al constructor, pero no a los otros objetos.*/
     private function __construct() {
-
+        echo "Personage creado." . PHP_EOL;
     }
 
     //Declara un método de creación estático público para obtener la instancia Singleton.
@@ -30,9 +31,19 @@ class Singleton {
         primera llamada y colocarlo dentro del campo estático. El método deberá devolver siempre esa instancia 
         en todas las llamadas siguientes.*/
         if(!isset(self::$variable)) {
-            self::$variable = new Singleton();
+            self::$variable = new Tigger();
         }
         return self::$variable;
+    }
+
+    public function roar() {
+        echo "Grhhhh!" . PHP_EOL;
+        self::$count++;
+    }
+
+    public function getCounter() {
+        $roar = self::$count;
+        echo "El numero de rugidos es de: " . $roar . PHP_EOL;
     }
 }
 
